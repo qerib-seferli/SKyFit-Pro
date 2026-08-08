74afea880530c100fd619eb814b2fda8c170a7e1
qerib-seferli <seferovqerib09@gmail.com>
2026-08-08 19:37:13 +0400

    Update layout.js

--- js/layout.js
+++ js/layout.js
@@ -1,16 +1,59 @@
-import { $, toggleTheme } from './core.js';
+import{
 
-export function initLayout() {
+$,
 
-  $('#themeBtn')?.addEventListener(
-    'click',
-    toggleTheme
-  );
+toggleTheme
 
-  $('#menuBtn')?.addEventListener(
-    'click',
-    () => {
-      $('#mobileMenu').classList.toggle('open');
-    }
-  );
+}
+
+from './core.js';
+
+export function initLayout(){
+
+$('#themeBtn')
+
+?.addEventListener(
+
+'click',
+
+toggleTheme
+
+);
+
+$('#menuBtn')
+
+?.addEventListener(
+
+'click',
+
+()=>{
+
+document.body
+
+.classList.toggle(
+
+'drawer-open'
+
+);
+
+});
+
+$('#drawerClose')
+
+?.addEventListener(
+
+'click',
+
+()=>{
+
+document.body
+
+.classList.remove(
+
+'drawer-open'
+
+);
+
+});
+
 }
