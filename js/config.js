bfb7e0a18a276c721f9741bf243d629e2ffaaddb
qerib-seferli <seferovqerib09@gmail.com>
2026-08-08 19:16:11 +0400

    Add configuration for Supabase and app settings

---
+++ js/config.js
@@ -0,0 +1,9 @@
+export const SUPABASE_URL = 'https://elpwornsvnplyzyufqir.supabase.co';
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVscHdvcm5zdm5wbHl6eXVmcWlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU4Mjc0NjgsImV4cCI6MjEwMTQwMzQ2OH0.9kxI4ZwUEJjwzVOweZowdAdlkAk9tUZ9rg7Yf7CnJJo';
+
+export const APP = {
+  NAME: 'SKy Fit',
+  VERSION: '1.0.0',
+};
+
+export const THEME_KEY = 'skyfit-theme';
